ca5ee5265c4cde97614c9edd6d4b44b221cc4082
Martin Fitzke <m.fitzke@gmx.net>
2019-11-28 20:34:10 +0100

    init4

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	fmt.Println("Hello")
 	p := types.Vector{1, 1, 1, 1}
 	fmt.Println(p.X)
-	v := types.NewTuple{1, 1, 1, 0}.(types.Vector)
+	v := types.Tuple{1, 1, 1, 0}
 	fmt.Println(v.W)
 }
