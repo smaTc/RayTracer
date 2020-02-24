80904c3469c788646eecaae81e9e8f2e584bbac2
Martin Fitzke <m.fitzke@gmx.net>
2020-02-24 14:31:01 +0100

    matrix operations file added

--- graphics/matrix.go
+++ graphics/matrix.go
@@ -244,29 +244,3 @@ func (m *Matrix) Scaling(x, y, z float32) interface{} {
 	scaleMat := Scaling(x, y, z)
 	return m.Multiply(scaleMat)
 }
-
-//Translation func
-func Translation(x, y, z float32) Matrix {
-	var mat Matrix
-	mat.GenerateMatrixWithDimension(4, 4)
-	mat.Values = [][]float32{
-		{1, 0, 0, x},
-		{0, 1, 0, y},
-		{0, 0, 1, z},
-		{0, 0, 0, 1},
-	}
-	return mat
-}
-
-//Scaling func
-func Scaling(x, y, z float32) Matrix {
-	var mat Matrix
-	mat.GenerateMatrixWithDimension(4, 4)
-	mat.Values = [][]float32{
-		{x, 0, 0, 0},
-		{0, y, 0, 0},
-		{0, 0, z, 0},
-		{0, 0, 0, 1},
-	}
-	return mat
-}
---
+++ graphics/operationsMatrix.go
@@ -0,0 +1,27 @@
+package graphics
+
+//Translation func
+func Translation(x, y, z float32) Matrix {
+	var mat Matrix
+	mat.GenerateMatrixWithDimension(4, 4)
+	mat.Values = [][]float32{
+		{1, 0, 0, x},
+		{0, 1, 0, y},
+		{0, 0, 1, z},
+		{0, 0, 0, 1},
+	}
+	return mat
+}
+
+//Scaling func
+func Scaling(x, y, z float32) Matrix {
+	var mat Matrix
+	mat.GenerateMatrixWithDimension(4, 4)
+	mat.Values = [][]float32{
+		{x, 0, 0, 0},
+		{0, y, 0, 0},
+		{0, 0, z, 0},
+		{0, 0, 0, 1},
+	}
+	return mat
+}
