46b29dba54742ecd0d6cda160c79cec66857ec7f
Martin Fitzke <m.fitzke@gmx.net>
2020-02-26 00:10:56 +0100

    comments etc

--- graphics/ray.go
+++ graphics/ray.go
@@ -6,15 +6,18 @@ type Ray struct {
 	Direction Vector
 }
 
+//NewRay func
 func NewRay(o Point, d Vector) Ray {
 	return Ray{Origin: o, Direction: d}
 }
 
+//SetRayProps func
 func (r *Ray) SetRayProps(o Point, d Vector) {
 	r.Origin = o
 	r.Direction = d
 }
 
+//Position func
 func (r *Ray) Position(t float32) Point {
 	vec := r.Direction.Scale(t)
 	newPoint, _ := r.Origin.Add(vec)
