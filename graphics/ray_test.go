5eac5dd6a7ce8956bc8cff95080a7f0d4ab83e12
Martin Fitzke <m.fitzke@gmx.net>
2020-02-26 01:03:34 +0100

    I REALLY HATE IT

--- graphics/ray_test.go
+++ graphics/ray_test.go
@@ -7,3 +7,12 @@ func Test(t *testing.T) {
 	r := NewRay(NewPoint(1, 2, 3), NewVector(4, 5, 6))
 
 }
+package graphics
+
+import "testing"	
+
+func Test(t *testing.T) {	
+
+	r := NewRay(NewPoint(1, 2, 3), NewVector(4, 5, 6))	
+
+}
